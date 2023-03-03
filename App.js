03854682e4ab54de51d990f5aa0fa9653a42951e
Roosep <roopes16@hotmail.com>
2023-03-03 16:30:29 +0200

    Finished app

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [address, setAddress] = useState("");
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+
+  const fetchAddress = () => {
+    fetch("http://www.mapquestapi.com/geocoding/v1/address?key=Th641oY1JqBxhPmRAjeQ5Kj2jCtEntWA&inFormat=kvp&outFormat=json&location="+address)
+    .then(response => response.json())
+    .then(data => {
+      const latLng = data.results[0].locations[0].latLng;
+      setLocation({latitude: latLng.lat, longitude: latLng.lng});
+    })
+    .catch(error => { 
+      Alert.alert("Error", error);
+    });
+  } 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={{ flex: 1, minWidth: 400, height: 650}} region={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+      }}>
+    <Marker coordinate={{
+      latitude: location.latitude,
+      longitude: location.longitude,
+      }} />
+  </MapView>
+      
+      <TextInput style={{width:200, borderColor: 'gray', borderWidth:1}} placeholder="Address" onChangeText={setAddress}  value={address} />
+      <Button title="Show" onPress={fetchAddress}/>
     </View>
   );
 }
